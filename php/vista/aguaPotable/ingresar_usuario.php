6e066fa78f0337b84fccbbde30d93e92c3194c1e
Michael Aimacana <maic_314@hotmail.com>
2022-12-22 16:34:31 -0500

    crear carpeta aguaPotable en vista

---
+++ php/vista/aguaPotable/ingresar_usuario.php
@@ -0,0 +1,222 @@
+<script>
+</script>
+<div class="col-sm-12">
+    <div class="box box-default color-palette-box">
+        <div class="box-header with-border">
+            <h3 class="box-title">Datos del Usuario</h3>
+        </div>
+        <form class="form-horizontal">
+            <div class="box-body">
+                <div class="row">
+                    <div class="col-sm-2">
+                        <label>Codigo Usuario:</label>
+                    </div>
+                    <div class="col-sm-4">
+                        <input type="text" name="TxtCodigoUsuario" id="TxtCodigoUsuario" class="form-control input-xs"
+                            value="">
+                    </div>
+                    <div class="col-sm-2">
+                        <label>Fecha de Ingreso:</label>
+                    </div>
+                    <div class="col-sm-4">
+                        <input type="text" name="TxtFechaIngreso" id="TxtFechaIngreso" class="form-control input-xs"
+                            value="">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-2">
+                        <label>Apellidos:</label>
+                    </div>
+                    <div class="col-sm-6">
+                        <input type="text" name="TxtApellidos" id="TxtApellidos" class="form-control input-xs" value="">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-2">
+                        <label>Nombres:</label>
+                    </div>
+                    <div class="col-sm-6">
+                        <input type="text" name="TxtNombres" id="TxtNombres" class="form-control input-xs" value="">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-2">
+                        <label>No. de Cedula:</label>
+                    </div>
+                    <div class="col-sm-2">
+                        <input type="text" name="TxtNCedula" id="TxtNCedula" class="form-control input-xs" value="">
+                    </div>
+                    <div class="col-sm-2">
+                        <label>No. de RUC:</label>
+                    </div>
+                    <div class="col-sm-2">
+                        <input type="text" name="TxtNRuc" id="TxtNRuc" class="form-control input-xs" value="">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-2">
+                        <label>Barrio:</label>
+                    </div>
+                    <div class="col-sm-6">
+                        <input type="text" name="TxtBarrio" id="TxtBarrio" class="form-control input-xs" value="">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-2">
+                        <label>Dirección:</label>
+                    </div>
+                    <div class="col-sm-6">
+                        <input type="text" name="TxtDireccion" id="TxtDireccion" class="form-control input-xs" value="">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-2">
+                        <label>Teléfono:</label>
+                    </div>
+                    <div class="col-sm-2">
+                        <input type="text" name="TxtTelefono" id="TxtTelefono" class="form-control input-xs" value="">
+                    </div>
+                    <div class="col-sm-2">
+                        <label>F. de Nacimiento:</label>
+                    </div>
+                    <div class="col-sm-2">
+                        <input type="text" name="TxtFNacimiento" id="TxtFNacimiento" class="form-control input-xs"
+                            value="">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-2">
+                        <label>Teléfono Celular:</label>
+                    </div>
+                    <div class="col-sm-2">
+                        <input type="text" name="TxtCelular" id="TxtCelular" class="form-control input-xs" value="">
+                    </div>
+                    <div class="col-sm-2">
+                        <label>Operadora:</label>
+                    </div>
+                    <div class="col-sm-2">
+                        <input type="text" name="TxtOperadora" id="TxtOperadora" class="form-control input-xs" value="">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-2">
+                        <label>Obs:</label>
+                    </div>
+                    <div class="col-sm-6">
+                        <input type="text" name="TxtObs" id="TxtObs" class="form-control input-xs" value="">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-3">
+                        <div class="box">
+                            <div class="radio">
+                                <label>
+                                    <input type="radio" name="optionsRadio" id="RbPN" value="option1" checked="">
+                                    Personas Naturales
+                                </label>
+                            </div>
+                            <div class="radio">
+                                <label>
+                                    <input type="radio" name="optionsRadio" id="RbIE" value="option2">
+                                    Instituciones Educativas
+                                </label>
+                            </div>
+                            <div class="radio">
+                                <label>
+                                    <input type="radio" name="optionsRadio" id="RbC" value="option3">
+                                    Comercios
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-3">
+                        <div class="box">
+                            <div class="radio">
+                                <label>
+                                    <input type="radio" name="optionsRadios" id="RbCSM" value="option1" checked="">
+                                    Con servicio y medidor
+                                </label>
+                            </div>
+                            <div class="radio">
+                                <label>
+                                    <input type="radio" name="optionsRadios" id="RbCSSM" value="option2">
+                                    Con servicio sin medidor
+                                </label>
+                            </div>
+                            <div class="radio">
+                                <label>
+                                    <input type="radio" name="optionsRadios" id="RbSCES" value="option3">
+                                    Solo contrato el servicio
+                                </label>
+                            </div>
+                            <div class="radio">
+                                <label>
+                                    <input type="radio" name="optionsRadios" id="RbMD" value="option3">
+                                    Medidor dañado
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-2">
+                        <div class="checkbox">
+                            <label>
+                                <input type="checkbox" id="ckC">
+                                Crédito
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-2">
+                        <div class="checkbox">
+                            <label>
+                                <input type="checkbox" id="ckA">
+                                Alcantarillado
+                            </label>
+                        </div>
+                    </div>
+                    <div class="col-sm-2">
+                        <div class="checkbox">
+                            <label>
+                                <input type="checkbox" id="ckTE">
+                                Tercera Edad
+                            </label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="box-footer">
+                <div class="row">
+                    <div class="col-sm-2">
+                        <label>Lectura del Medidor:</label>
+                    </div>
+                    <div class="col-sm-2">
+                        <input type="text" name="TxtLecturaMedidor" id="TxtLecturaMedidor" class="form-control input-xs"
+                            value="">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-4">
+                        <button type="submit" class="btn btn-app">
+                            <i class="glyphicon glyphicon-list-alt"></i> Nuevo
+                        </button>
+
+                        <button type="submit" class="btn btn-app">
+                            <i class="glyphicon glyphicon-remove"></i> Cancelar
+                        </button>
+
+                        <button type="submit" class="btn btn-app">
+                            <i class="glyphicon glyphicon-pencil"></i> Modificar
+                        </button>
+
+                        <button type="submit" class="btn btn-app">
+                            <i class="glyphicon glyphicon-log-out"></i> Salir
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
