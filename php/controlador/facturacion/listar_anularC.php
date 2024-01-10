10d218b95b43b04fd73b4581ba1dbc1843c24453
javo <javier.farinango92@gmail.com>
2024-01-10 17:36:17 -0500

    cambio jav

--- php/controlador/facturacion/listar_anularC.php
+++ php/controlador/facturacion/listar_anularC.php
@@ -9,6 +9,7 @@ $controlador =  new listar_anularC();
 if(isset($_GET['DCTipo']))
 {
 	// $parametros = $_POST['parametros'];
+	print_r('expression');die();
 	echo json_encode($controlador->DCTipo());
 }
 if(isset($_GET['DCSerie']))
@@ -101,7 +102,6 @@ class listar_anularC
 
 	function DCTipo()
 	{
-		print_r('holaaa');die();
 		$datos = $this->modelo->DCTipo();
 		
 		$tc = array();
