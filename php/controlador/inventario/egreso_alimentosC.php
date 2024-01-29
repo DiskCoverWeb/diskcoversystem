dbde1d8a57b3b2f6405ce7fb22d071c4cb19497a
javo <javier.farinango92@gmail.com>
2024-01-29 12:01:54 -0500

    cambios

--- php/controlador/inventario/egreso_alimentosC.php
+++ php/controlador/inventario/egreso_alimentosC.php
@@ -30,6 +30,20 @@ if(isset($_GET['buscar_producto']))
 	$parametros = $_POST['parametros'];
 	echo json_encode($controlador->buscar_producto($parametros));
 }
+if(isset($_GET['add_egresos']))
+{
+	$parametros = $_POST['parametros'];
+	echo json_encode($controlador->add_egresos($parametros));
+}
+if(isset($_GET['listar_egresos']))
+{
+	echo json_encode($controlador->listar_egresos());
+}
+if(isset($_GET['eliminar_egreso']))
+{
+	$id = $_POST['id'];
+	echo json_encode($controlador->eliminar_egreso($id));
+}
 
 /**
  * 
@@ -70,9 +84,55 @@ class egreso_alimentosC
 
 	function buscar_producto($parametros)
 	{
-		$datos = $this->modelo->buscar_producto();
+		$datos = $this->modelo->buscar_producto($parametros['codigo']);
 		return $datos;
+	}
+
+	function add_egresos($parametros)
+	{
+		$producto = $this->modelo->buscar_producto(false,$parametros['id']);
+		$data = $producto[0];
+
+		SetAdoAddNew('Trans_Kardex');
+	    SetAdoFields('T','S');
+	    SetAdoFields('Salida',$parametros['cantidad']);
+	    SetAdoFields('CodBodega',$data['CodBodega']);
+	    SetAdoFields('Codigo_Barra',$data['Codigo_Barra']);
+	    SetAdoFields('Codigo_Inv',$data['Codigo_Inv']);	
+	    SetAdoFields('Fecha',$parametros['fecha']);	
+	    SetAdoFields('Codigo_P',$data['Codigo_P']);	
+	    SetAdoFields('Orden_No',$data['Orden_No']);	
+	    SetAdoFields('CodigoU',$data['CodigoU']);	
+	    SetAdoFields('Valor_Unitario',$data['Valor_Unitario']);	
+	    SetAdoFields('Valor_Total',number_format(floatval($data['Valor_Unitario'])*floatval($parametros['cantidad']),2,'.',''));	
+	     SetAdoFields('Total',number_format(floatval($data['Valor_Unitario'])*floatval($parametros['cantidad']),2,'.',''));	
+	    SetAdoFields('Periodo',$_SESSION['INGRESO']['periodo']);	
+	    SetAdoFields('Item',$_SESSION['INGRESO']['item']);		
+	   return  SetAdoUpdate();		
+
 	}	
+
+	function listar_egresos()
+	{
+		$tr = '';
+		$datos = $this->modelo->buscar_producto_egreso();
+		foreach ($datos as $key => $value) {
+			$tr.='<tr>			
+			<td>'.($key+1).'</td>
+			<td>'.$value['Fecha']->format('Y-m-d').'</td>
+			<td>'.$value['Producto'].'</td>
+			<td>'.$value['Salida'].'</td>
+			<td><button type="button" class="btn btn-danger btn-sm" onclick="eliminar_egreso('.$value['ID'].')"><i class="fa fa-trash"></i></button></td>
+			</tr>';
+		}
+		return $tr;
+	}
+
+	function eliminar_egreso($id)
+	{
+		return $this->modelo->eliminar($id);
+		// print_r($id);die();
+	}
 }
 
 
