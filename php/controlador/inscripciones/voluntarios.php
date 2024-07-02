4f1ef9f04bdba139fddd6fc7c44bebdfd1ea348d
alejandromoreira2002 <tedalemorvel@gmail.com>
2024-07-02 00:58:56 -0500

    pantalla inscripciones actualizado

--- php/controlador/inscripciones/voluntarios.php
+++ php/controlador/inscripciones/voluntarios.php
@@ -6,8 +6,63 @@
     if(isset($_GET['CatalogoForm'])){
         echo json_encode($controlador->getCatalogo());
     }
-    if(isset($_GET['EnviarInscripcion'])){
-        echo json_encode($controlador->enviarInscripcion($_POST['parametros']));
+    if(isset($_GET['ConsultarCliente'])){
+        $cedula = $_POST['cedula'];
+        echo json_encode($controlador->consultarCliente($cedula));
+    }
+    if(isset($_GET['EnviarInscripcion']) && isset($_GET['ModoEnviar'])){
+        $parametros = $_POST;
+        $modo = $_GET['ModoEnviar'];
+        $nombresArchivos = array();
+        if(count($_FILES) > 0){
+            foreach($_FILES as $key => $file){
+                $filePathBase = dirname(__DIR__, 2) . "/comprobantes/inscripciones/";
+                
+                if (!is_dir($filePathBase)) {
+                    mkdir($filePathBase, 0777, true);
+                }
+    
+                if(isset($_FILES[$key]) && $_FILES[$key]['error'] == UPLOAD_ERR_OK){
+                    $ext = strtolower(pathinfo($file['name'], PATHINFO_EXTENSION));
+                    $newFileName = '';
+    
+                    switch($key){
+                        case 'Archivo_CI_RUC_PAS':
+                            $newFileName = 'cedula';
+                            break;
+                        case 'Archivo_Record_Policial':
+                            $newFileName = 'record';
+                            break;
+                        case 'Archivo_Planilla':
+                            $newFileName = 'plansb';
+                            break;
+                        case 'Archivo_Carta_Recom':
+                            $newFileName = 'carta';
+                            break;
+                        case 'Archivo_Certificado_Medico':
+                            $newFileName = 'medico';
+                            break;
+                        case 'Archivo_VIH':
+                            $newFileName = 'vih';
+                            break;
+                        case 'Archivo_Reglamento':
+                            $newFileName = 'regbaq';
+                            break;
+                    }
+    
+                    $filename = $parametros['CI_RUC'] . $newFileName . '.' . $ext;
+                    $filepath = $filePathBase . $filename;
+    
+                    if (move_uploaded_file($file['tmp_name'], $filepath)) {
+                        $parametros[$key] = $filename;
+                    } else {
+                        echo json_encode(["codigo" => 0, "respuesta" => "No se ha cargado el archivo: " . $file['name']]);
+                        return;
+                    }
+                }
+            }
+        }
+        echo json_encode($controlador->enviarInscripcion($parametros, $modo));
     }
 
     class InscVoluntariosC{
@@ -40,8 +95,42 @@
             }
         }
 
-        function enviarInscripcion($parametros){
-            $respuesta = $this->modelo->enviarInscripcion($parametros);
+        function consultarCliente($cedula){
+            $voluntario = $this->modelo->consultarCliente($cedula);
+            if(count($voluntario) > 0){
+                $datos = array();
+                $archivos = array();
+                foreach($voluntario[0] as $key => $value){
+                    if(str_contains($key, "Archivo_")){
+                        $archivos[$key] = $value;
+                    }else{
+                        $datos[$key] = $value;
+                    }
+                }
+                $respuesta = array(
+                    'res' => 1,
+                    'datos' => array(
+                        'datos' => $datos,
+                        'archivos' => $archivos
+                    )
+                );
+                return $respuesta;
+            }else{
+                $respuesta = array(
+                    'res' => 0,
+                    'datos' => 'No hay cedula registrada'
+                );
+                return $respuesta;
+            }
+        }
+
+        function enviarInscripcion($parametros, $modo){
+            $respuesta = 0;
+            if($modo=="CrearVoluntario"){
+                $respuesta = $this->modelo->crearInscripcion($parametros);
+            }else{
+                $respuesta = $this->modelo->actualizarInscripcion($parametros);
+            }
 
             if($respuesta == 1){
                 return array(
--- php/modelo/inscripciones/voluntarios.php
+++ php/modelo/inscripciones/voluntarios.php
@@ -1,5 +1,6 @@
 <?php 
     require_once(dirname(__DIR__, 2) . "/db/db1.php");
+    require_once(dirname(__DIR__,2)."/funciones/funciones.php");
     @session_start();
 
     class InscVoluntariosM{
@@ -18,67 +19,42 @@
             return $this->db->datos($sql);
         }
 
-        function enviarInscripcion($param){
-            $sql = "INSERT INTO Clientes
-            (T,FA,Codigo,Cliente,Telefono,CI_RUC,Sexo,Fecha_N,Plan_Afiliado,Est_Civil,Calificacion,Gestacion,Especial,Referencia,Dosis,Asignar_Dr,DireccionT,Representante,CodigoA,Contacto,Telefono_R,Tipo_Cta,Canton,Parroquia,Barrio,Direccion,DirNumero,Credito,No_Dep,Matricula,Cod_Banco,Descuento,Profesion,Porc_C,FAX,FactM,Casilla,Cod_Ejec,Cta_CxP,Lugar_Trabajo,Tipo_Cliente,Bono_Desarrollo,IESS,Actividad,Tipo_Vivienda,Servicios_Basicos,Archivo_CI_RUC_PAS,Archivo_Record_Policial,Archivo_Planilla,Archivo_Carta_Recom,Archivo_Certificado_Medico,Archivo_VIH,Archivo_Reglamento,TB,TD)
-            VALUES(
-            '.',
-            0,
-            '".$param['cedula']."',
-            '".$param['cliente']."',
-            '".$param['telefono']."',
-            '".$param['cedula']."',
-            '".$param['genero']."',
-            '".$param['fecha_nacimiento']."',
-            '".$param['ciudadania']."',
-            '".$param['estado_civil']."',
-            '".$param['estado_lactancia']."',
-            '".$param['estado_gestacion']."',
-            ".$param['toma_medicina'].",
-            '".$param['medicamento']."',
-            '".$param['dosis']."',
-            ".$param['alergia'].",
-            '".$param['prod_alergia']."',
-            '".$param['nombre_conyugue']."',
-            '".$param['cedula_conyugue']."',
-            '".$param['nombre_emergencia']."',
-            '".$param['telefono_emergencia']."',
-            '".$param['parentesco_emergencia']."',
-            '".$param['canton']."',
-            '".$param['parroquia']."',
-            '".$param['barrio']."',
-            '".$param['direccion']."',
-            '".$param['num_casa']."',
-            ".$param['personas_domicilio'].",
-            ".$param['num_hijos'].",
-            ".$param['hijos_mayores'].",
-            ".$param['hijos_menores'].",
-            ".$param['discapacidad'].",
-            '".$param['tipo_discapacidad']."',
-            ".$param['porc_discapacidad'].",
-            '".$param['conadis']."',
-            ".$param['familiar_discapacidad'].",
-            '".$param['parentesco_fdiscapacidad']."',
-            '".$param['conadis_familiar']."',
-            '".$param['enfermedad']."',
-            '".$param['nivel_estudio']."',
-            '".$param['ocupacion']."',
-            ".$param['bono'].",
-            ".$param['jubilacion'].",
-            '".$param['actividad_economica']."',
-            '".$param['vivienda']."',
-            '".$param['servicios_basicos']."',
-            '".$param['cedula_pdf']."',
-            '".$param['record_policial']."',
-            '".$param['planilla_sbasico']."',
-            '".$param['carta_recomendacion']."',
-            '".$param['certificado_medico']."',
-            '".$param['prueba_vih']."',
-            '".$param['reglamento_baq']."',
-            'INSC',
-            'C');";
+        function consultarCliente($cedula){
+            $sql = "SELECT Codigo,Cliente,Telefono,CI_RUC,Sexo,Fecha_N,Plan_Afiliado,Est_Civil,Calificacion,Gestacion,Especial,Referencia,Dosis,Asignar_Dr,DireccionT,Representante,CodigoA,Contacto,Telefono_R,Tipo_Cta,Canton,Parroquia,Barrio,Direccion,DirNumero,Credito,No_Dep,Matricula,Cod_Banco,Descuento,Profesion,Porc_C,FAX,FactM,Casilla,Cod_Ejec,Cta_CxP,Lugar_Trabajo,Tipo_Cliente,Bono_Desarrollo,IESS,Actividad,Tipo_Vivienda,Servicios_Basicos,Archivo_CI_RUC_PAS,Archivo_Record_Policial,Archivo_Planilla,Archivo_Carta_Recom,Archivo_Certificado_Medico,Archivo_VIH,Archivo_Reglamento
+            FROM Clientes
+            WHERE CI_RUC = '".$cedula."'
+            AND TB = '93.03'";
+            return $this->db->datos($sql);
+        }
 
-            return $this->db->String_Sql($sql);
+        function crearInscripcion($param){
+            SetAdoAddNew("Clientes");
+            foreach($param as $key => $value){
+                SetAdoFields($key, $value);
+            }
+            SetAdoFields("T", '.');
+            SetAdoFields("FA", 0);
+            SetAdoFields("Codigo", $param['CI_RUC']);
+            SetAdoFields("TD", "C");
+            SetAdoUpdate();
+            return 1;
+        }
+
+        function actualizarInscripcion($param){
+            $TB = $param['TB'];
+            $cedula = $param['CI_RUC'];
+            
+            unset($param['TB']);
+            unset($param['CI_RUC']);
+
+            SetAdoAddNew("Clientes");
+            foreach($param as $key => $value){
+                SetAdoFields($key, $value);
+            }
+            SetAdoFieldsWhere("TB", $TB);
+            SetAdoFieldsWhere("CI_RUC", $cedula);
+            SetAdoUpdateGeneric();
+            return 1;
         }
     }
 ?>
