aa85956943dba9c6540a190274c8e6928b12fb22
alejandromoreira2002 <tedalemorvel@gmail.com>
2024-07-17 14:28:11 -0500

    pequenos arreglos

--- dist/js/inventario/catalogo_bodega.js
+++ dist/js/inventario/catalogo_bodega.js
@@ -590,14 +590,14 @@ function tipoProceso() {
                         $('#txtConcepto').attr('placeholder', '');
                         $('#pictureContainer').css('display', 'block');
                         $('#reqFacturaContainer').css('display', 'block');
-                        $('#checkboxContainer').css('display', 'none');
+                        $('#checkboxContainer').css('display', 'block');
                         $('#colorContainer').css('display', 'block');
                         break;
                     case '00':
                         $('#txtConcepto').attr('placeholder', '');
                         $('#tp').val('CATEGORI');
-                        $('#pictureContainer').css('display', 'none');
-                        $('#reqFacturaContainer').css('display', 'none');
+                        $('#pictureContainer').css('display', 'block');
+                        $('#reqFacturaContainer').css('display', 'block');
                         $('#checkboxContainer').css('display', 'block');
                         $('#colorContainer').css('display', 'block');
                         break;
