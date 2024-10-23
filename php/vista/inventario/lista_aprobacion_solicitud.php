5d37973e4700e3c09aa210862591e26d42b39588
javo <javier.farinango92@gmail.com>
2024-10-23 14:01:13 -0500

    cambios

--- php/vista/inventario/lista_aprobacion_solicitud.php
+++ php/vista/inventario/lista_aprobacion_solicitud.php
@@ -106,7 +106,6 @@ function EliminarSolicitud(orden)
 					<th>Contratista</th>
 					<th>Orden</th>
 					<th>Fecha Solicitud</th>
-					<th>Fecha Entrega</th>
 					<th>Presupuesto</th>
 					<th></th>
 				</thead>
