99a0c0fa679289f1c0b55f23c89f0c1e751fa34c
javo <javier.farinango92@gmail.com>
2024-10-23 14:24:00 -0500

    ejemplo

--- php/controlador/inventario/solicitud_materialC.php
+++ php/controlador/inventario/solicitud_materialC.php
@@ -639,8 +639,8 @@ class solicitud_materialC
 					<td>'.($key+1).'</td>
 					<td>'.$value['Codigo_Inv'].'</td>
 					<td>'.$value['Producto'].'</td>
-					<td>2</td>
 					<td>100</td>
+					<td>2 </td>
 					<td width="20px">
 					<input type="text" id="txt_cant_'.$value['ID'].'" name="txt_cant_'.$value['ID'].'" value="'.$value['Cantidad'].'" class="form-control input-sm"></td>
 
