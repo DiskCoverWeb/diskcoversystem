8c7a159ee5638aa95f8e4a428f1ce7aebd175cfa
javo1992 <javier.farinango92@gmail.com>
2024-10-28 16:59:10 -0500

    cambios

--- lib/phpmailer/EnvioEmailvisual.php
+++ lib/phpmailer/EnvioEmailvisual.php
@@ -64,7 +64,7 @@ class EnviarVisual
 
 	        try {
 		          //Server settings
-		          $mail->SMTPDebug = SMTP::DEBUG_SERVER;                 //Enable verbose debug output
+		          // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                 //Enable verbose debug output
 		            $mail->isSMTP(); //Send using SMTP
 		            // $mail->Helo = 'smtp.diskcoversystem.com';    
 				    $mail->Host = 'imap.diskcoversystem.com';
